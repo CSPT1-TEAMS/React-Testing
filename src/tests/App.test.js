c6049696e58e755b097f29d066ccfdacb87d0069
Alexander <alexander.botello@g.austincc.edu>
2018-07-10 22:33:10 -0500

    Add tests for App Component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import App from '../App';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<App />', () => {
+  const wrapper = shallow(<App />)
+  const instance = wrapper.instance();
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
+
+  it('should render a Display component', () => {
+    expect(wrapper.find('Display').length).toBe(1);
+  })
+
+  it('should render a Panel component', () => {
+    expect(wrapper.find('Panel').length).toBe(1);
+  })
+
+  it('should be able to change state of operation', () => {
+    instance.handleClick('-');
+    expect(instance.state.operation).toBe('-');
+  })
+
+  it('should be able to change state of next', () => {
+    instance.handleClick('5');
+    expect(instance.state.next).toBe('5');
+  })
+
+  it('should be able to change state of total', () => {
+    const wrapper = shallow(<App />)
+    const instance = wrapper.instance();
+    instance.handleClick('5');
+    instance.handleClick('+');
+    instance.handleClick('10');
+    instance.handleClick('=');
+    expect(instance.state.total).toBe('15');
+  })
+
+  // it should reset back to intial state when AC is pressed
 });
