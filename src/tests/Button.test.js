28415714fe03e7d17b4e3c1f59bf725d7f2b1415
Alexander <alexander.botello@g.austincc.edu>
2018-07-10 21:50:27 -0500

    Add tests for Button Component

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Button from '../components/Button/Button';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<Button />', () => {
+  const props = {
+    clickHandler: jest.fn(),
+    orange: true,
+    wide: true,
+    name: '5',
+  }
+  const wrapper = shallow(<Button {...props} />)
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
   });
+
+  it('should render a button', () => {
+    expect(wrapper.find('button')).toHaveLength(1);
+  })
+
+  it('should call handleClick function when button is clicked', () => {
+    const button = wrapper.find('button');
+    button.simulate('click')
+    expect(props.clickHandler).toHaveBeenCalledTimes(1);
+  })
+
+  it('should add the css class orange to the button', () => {
+    expect(wrapper.find('.orange')).toHaveLength(1);
+  })
+
+  it('should add the css class wide to the button', () => {
+    expect(wrapper.find('.wide')).toHaveLength(1);
+  })
+
+  it('should render the name prop passed', () => {
+    const button = wrapper.find('button')
+    expect(button.text()).toMatch(props.name);
+  })
 });
