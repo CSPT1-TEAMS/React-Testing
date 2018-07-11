048d474d342b1122661b511dbb2ccb2caf107ac2
Alexander <alexander.botello@g.austincc.edu>
2018-07-10 22:07:20 -0500

    Add tests for Panel Component

--- src/tests/Panel.test.js
+++ src/tests/Panel.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import Panel from '../components/Panel/Panel';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Panel />', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Panel />, div);
   });
+
+  it('should render all buttons', () => {
+    const wrapper = shallow(<Panel />)
+    expect(wrapper.find('Button').length).toBe(19);
+  })
 });
