bdac18c30a05937fb4947320bc9f9452f24e39aa
Alexander <alexander.botello@g.austincc.edu>
2018-07-10 21:13:46 -0500

    Add div test to Display

--- src/tests/Display.test.js
+++ src/tests/Display.test.js
@@ -5,16 +5,21 @@ import Display from '../components/Display/Display';
 
 
 describe('<Display /> Component', () => {
+  const props = {
+    value: "Render this string"
+  }
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Display />, div);
   });
 
   it('should render the value prop', () => {
-    const props = {
-      value: "Render this string"
-    }
     const wrapper = shallow(<Display {...props} />);
     expect(wrapper.text()).toMatch(props.value);
+  })
+
+  it('should render a div', () => {
+    const wrapper = shallow(<Display {...props} />)
+    expect(wrapper.type()).toBe('div')
   })
 });
